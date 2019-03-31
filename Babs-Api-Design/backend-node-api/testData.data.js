ec071764d7a73f92fb4b37170f4db74cbfc408ff
babanila <babanila@yahoo.com>
2019-03-31 15:58:02 +0200

    test(testData.data.js): add sample data for test

---
+++ Babs-Api-Design/backend-node-api/testData.data.js
@@ -0,0 +1,32 @@
+const sampleData = {
+  numberOfItems: 2372,
+  items: [
+    {
+      id: 57965046,
+      name: "BABaseProject",
+      html_url: "https://github.com/boai/BABaseProject"
+    },
+    {
+      id: 43004481,
+      name: "Babadook",
+      html_url: "https://github.com/jseidl/Babadook"
+    },
+    {
+      id: 120905144,
+      name: "babar",
+      html_url: "https://github.com/criteo/babar"
+    },
+    {
+      id: 9783304,
+      name: "babar",
+      html_url: "https://github.com/gigasquid/babar"
+    },
+    {
+      id: 150472224,
+      name: "babagkachatbox",
+      html_url: "https://github.com/knakul853/babagkachatbox"
+    }
+  ]
+};
+
+exports.sampleData = sampleData;
